aae3f4d424fe9e6300505a160987416c708d5600
HuiPeng <huipeng1982@gmail.com>
2014-10-13 10:37:33 +0800

    change get to post

--- routes/index.js
+++ routes/index.js
@@ -28,12 +28,11 @@ router.post('/', function(req, res) {
         var result = sort.sorting(mail, pick.picking);
 
         request.post(
-            'http://127.0.0.1:3000/imports/order',
-            { result: result },
+            'http://127.0.0.1:3000/',
+            { orders: result },
             function (error, response, body) {
                 if (!error && response.statusCode == 200) {
-                    res.writeHead(200, {'content-type': 'text/plain'});
-                    res.end();
+                    res.status(200).end();
                 }
             }
         );
